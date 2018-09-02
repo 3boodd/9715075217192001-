32ac796ce8ddd736be2ace02ce8bd55314e6aa4c
3boodd <36028899+3boodd@users.noreply.github.com>
2018-09-02 22:19:37 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,4 +3,7 @@ const client = new Discord.Client();
 const fs = require("fs");
 client.on(`ready`, () => console.log(`Ready!`))
 const channels = {};
+
+
  
+client.login(process.env.BOT_TOKEN);  //اياكككك تلعب هنا لا تحط توكنك هنا
