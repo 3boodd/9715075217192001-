b3115205e7ab3db6654e631efb250d4d0c7a136c
3boodd <36028899+3boodd@users.noreply.github.com>
2018-09-04 02:49:25 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,14 +66,6 @@ client.on('message', message => {
 //========================================================================Other
 
 
-  client.on('message', async message => {
-if(message.author.bot) return;
-if (message.channel.guild) {
-if (message.content === -help') {
-message.author.send(`الرسآلهه`).catch(RebeL =>{console.log('`Error`: ' + RebeL);
-message.channel.send("**للأسف , لديك اعدادات خصوصية لاتسمح لي بأرسال رسائل خاصة لك **")
-});
-}}});
   
 
 client.on('message', message => {
@@ -167,43 +159,6 @@ var mentionned = message.mentions.members.first();
 
          
      });
-  
-
-  const afk = require('./afk.json');
-client.on('message',async rebel => {
-      if(rebel.author.bot) return;
-  if (afk[rebel.author.id]) {
-    delete afk[rebel.author.id];
-    if (rebel.member.nickname === null) {
-      msg.channel.send("أهلا بك , <@"+rebel.author.id+"> أنأ **أحاول** أن أزيل عنك الأفك.");     } else {
-      rebel.member.setNickname(rebel.member.nickname.replace(/(\[AFK\])/,''));
-      rebel.channel.send("أهلا بك , <@"+rebel.author.id+"> تم فك عنك الأفك بسبب رجوعك."); 
-    }
-    fs.writeFile("./afk.json", JSON.stringify(afk), (err) => {if (err) console.error(err);});
-} else {
-    if (rebel.content.startsWith(prefix + 'afk ')||rebel.content === prefix + 'afk') {
-      rebel.member.setNickname("[AFK] " + rebel.member.displayName);
-      let args1 = rebel.content.split(' ').slice(1);
-      if (args1.length === 0) {
-        afk[rebel.author.id] = {"reason": true}; 
-        rebel.reply("** لقد وضعتك بوضع الأفك **")
-      } else {
-        afk[rebel.author.id] = {"reason": args1.join(" ")}; // with reason
-        rebel.reply("لقد أضفتك للأفك بسبب "+ args1.join(" ") + ".")
-      }
-      fs.writeFile("./afk.json", JSON.stringify(afk), (err) => {if (err) console.error(err);});   
-  }
-}
-         var mentionned = rebel.mentions.users.first();
-if(rebel.mentions.users.size > 0) return ;
-if (afk[rebel.mentions.users.first().id]) {
-if (afk[rebel.mentions.users.first().id].reason === true) {
-rebel.channel.send(`**<@!${mentionned.id}> مأفك** `);
-}else{
-rebel.channel.send(`**<@!${mentionned.username}> مأفك , سبب الأفك \n ${afk[rebel.mentions.users.first().id].reason}**`);
-}
-} 
-});
   
 
 
